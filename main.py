76c510ef44ab517d68c28cdb50f6ae101c36c821
Dorothy Kunth <dorothykunth@gmx.de>
2023-06-21 20:57:13 +0200

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src.load_data_to_s3 import df_to_s3
 
 
 # from dotenv import load_dotenv
-# load_env() # only for local
+# load_dotenv() # only for local testing
 
 
 # import variables from .env file
