c8c598430f690e8bb0efb1216877868ae45a92fa
Dorothy Kunth <dorothykunth@gmx.de>
2023-06-21 13:25:15 +0200

    commit message

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+from datetime import datetime
+from src.extract import extract_transaction_data
+from src.transform import identify_and_remove_duplicated_data
+from src.load_data_to_s3 import df_to_s3
+
+
+# from dotenv import load_dotenv
+# load_env() # only for local
+
+
+# import variables from .env file
+dbname = os.getenv("dbname")
+host = os.getenv("host")
+port = os.getenv("port")
+user = os.getenv("user")
+password = os.getenv("password")
+aws_access_key_id = os.getenv("aws_access_key_id")
+aws_secret_access_key_id = os.getenv("aws_secret_access_key_id")
+
+# this creates a variable that tracks the time we executed the script
+start_time = datetime.now()
+
+# make a connection to redshift and extract online transactions data with transformation tasks
+print("\nExtracting and transforming data in sql...")
+online_trans_cleaned = extract_transaction_data(dbname, host, port, user, password)
+print('\nExtraction and transformation in sql completed')
+
+# remove duplicated data
+print("\nRemoving duplicated data...")
+online_trans_deduped = identify_and_remove_duplicated_data(online_trans_cleaned)
+print('\nDuplicated data removed')
+
+# load online_trans_deduped to s3
+print("\nLoading data to s3...")
+s3_bucket = 'waia-data-dump'
+key = 'bootcamp2/etl/dk_online_trans_cleaned.csv'
+
+df_to_s3(online_trans_deduped, key, s3_bucket, aws_access_key_id, aws_secret_access_key_id)
+print('\nData loaded to s3')
+
+# this creates a variable that calculates how long it takes to run the script
+execution_time = datetime.now() - start_time
+print(f"\nTotal execution time (hh:mm:ss.ms) {execution_time}")
